3cfbf30610a50da8ccb01920339eec689d26cc88
XxtraBlood <tristankrizanpoop@gmail.com>
2024-01-11 10:59:56 -0800

    added files for pages and set up NextUI

---
+++ src/app/newsletter/page.tsx
@@ -0,0 +1,9 @@
+const Newsletter = () => {
+  return (
+    <div>
+      <h1>Join newsletter Page</h1>
+    </div>
+  );
+};
+
+export default Newsletter;
---
+++ src/app/remind/page.tsx
@@ -0,0 +1,9 @@
+const Remind = () => {
+  return (
+    <div>
+      <h1>Remind page</h1>
+    </div>
+  );
+};
+
+export default Remind;
